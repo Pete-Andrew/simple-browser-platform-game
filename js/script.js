24034c69feedfc46cd4c8fd46e00fc6de4d1ba6e
Pete Andrew <112977421+Pete-Andrew@users.noreply.github.com>
2022-11-14 13:07:29 +0000

    update 3

--- js/script.js
+++ js/script.js
@@ -4,6 +4,14 @@ const c = canvas.getContext('2d')
 canvas.width = 64 * 16 //1024
 canvas.height = 64 * 9 //576
 
+const backgroundLevel1 = new Sprite ({
+    position: {
+        x: 0,
+        y: 0,
+    },
+    imageSrc: './img/backgroundLevel1.png'
+})
+
 const player = new Player()
 
 const keys = {
@@ -20,18 +28,16 @@ const keys = {
 
 function animate() {
     window.requestAnimationFrame(animate)
-    //console.log('go') //shows that the function is runninng in console
     
-    c.fillStyle = 'white'
-    c.fillRect(0, 0, canvas.width, canvas.height)
-    
-
+    backgroundLevel1.draw()
+   
     player.velocity.x = 0 //starts the player with x axis movement =0
     if (keys.d.pressed) player.velocity.x = 5
     else if (keys.a.pressed) player.velocity.x = -5
       
     player.draw()
     player.update()
+    
 } 
 
 animate()
